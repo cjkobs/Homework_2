85977b361d472c0ed94114a6958fa8de41addf6c
cjkobs <ckobs@asu.edu>
2022-09-18 07:36:44 -0700

    starting hw2 in pycharm

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from scipy.optimize import minimize
+import numpy as np
+# function, fun, that we want to solve for(minimize)
+fun = lambda x: (x[0]+1)**2 + (x[1])**2 + (x[2]-1)**2
+# initial guess since that is necessary to start
+xinit = [-1, 0, 1]
+# constraints based on solving the form given
+constr = ({"type": "eq", "fun": lambda x: x[0] + 2*x[1] + 3*x[2] - 1})
+# no bounds to be set for the problem
+res = minimize(fun, xinit, method= 'SLSQP', constraints=constr)
+# print the answer
+print(res.x)
