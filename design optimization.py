acf2a277ba67068e82b04674269915b9a3378bfe
cjkobs <ckobs@asu.edu>
2022-09-18 15:37:19 -0700

    working my way through it, yikes

--- design optimization.py
+++ design optimization.py
@@ -1,4 +1,11 @@
 import numpy as np
 
-def gradient_descent(g, x0, learn_rate, iterations, tolerance)
-vector = start
+def gradient_descent(g, x0, learn_rate, iterations, tolerance
+                     ):
+    vector = start
+    for _ in range(iterations):
+        diff = -learn_rate * gradient(vector)
+        if np.all(np.abs(diff) <= tolerance or iterations > 100):
+            break
+        vector += diff
+    return vector
