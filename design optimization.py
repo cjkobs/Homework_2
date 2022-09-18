4c30d39c1247407c80ba3965d7651049412c2754
cjkobs <ckobs@asu.edu>
2022-09-18 15:28:26 -0700

    working my way through it, yikes

---
+++ design optimization.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def gradient_descent(g, x0, learn_rate, iterations, tolerance)
+vector = start
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from scipy.optimize import minimize
+from scipy.optimize import minimize, fmin_cg
+import matplotlib as plt
 import numpy as np
+
+# This section is part a
 # function, fun, that we want to solve for(minimize)
 fun = lambda x: (x[0]+1)**2 + (x[1])**2 + (x[2]-1)**2
 # initial guess since that is necessary to start
@@ -10,3 +13,7 @@ constr = ({"type": "eq", "fun": lambda x: x[0] + 2*x[1] + 3*x[2] - 1})
 res = minimize(fun, xinit, method= 'SLSQP', constraints=constr)
 # print the answer
 print(res.x)
+
+# This section is part b, pretty sure this is what it's asking for
+fun = lambda x: (2-2*x[0]-3*x[1])**2 + (x[0])**2 + (x[1]-1)**2
+# checked the slack and saw we are supposed to write our own gradient descent algorithm
