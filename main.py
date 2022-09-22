a83623cfe034549a021b155bca1f2517f554a5d3
cjkobs <ckobs@asu.edu>
2022-09-21 19:46:21 -0700

    it's not pretty and it's not done but hey, at least it's something

--- main.py
+++ main.py
@@ -19,18 +19,37 @@ print(fun)
 # god how did it take me this long to just hardcode the gradient in, whatever here are the gradient and hessian
 grad = lambda x: [(-8 + 10 * x[0] + 12 * x[1]), (-14 + 12 * x[1] + 20 * x[2])]
 print(grad)
-gradt = grad.transpose(grad)
+gradt = lambda x: [[-8 + 10 * x[0] + 12 * x[1]], [-14 + 12 * x[1] + 20 * x[2]]]
 print(gradt)
 hess = [[10, 12], [12, 20]]
 print(hess)
 
+# setting up the algorithm that was discussed in class when doing inexact line search
+# chose 0.5 since it's case dependent and it was right in the middle
 t = 0.5
+x_val=[]
 def phi(x, alph):
     return fun - (t * np.matmul(gradt, grad) * alph)
 def new_alph(x):
     alph = 1
     k = 0
-    while fun(x - alph * grad) > phi(x,alph) and k < 100:
-        alph = 0.5* alph
+    while fun(x - alph * grad) > phi(x, alph) and k < 100:
+        alph = 0.5 * alph
         k = k + 1
     return alph
+# I tried adding an else break condition at the end but it was a pain, gonna ask my CS buddy if he knows how
+
+# So what I was trying to do was define my minimization function, the gradient of that function and the hessian
+# from there, I made the functions that we talked about in lecture 7 while going through the inexact line search
+# The problem is I'm not sure what to do with them especially since it won't let me print new_alph
+# I'm guessing it's because it's a function and not a variable in which case it would need initial conditions?
+# That's above my pay grade, hopefully the code for how to do a simple problem like this will be posted so I can
+# hopefully get better at coding for the future.
+
+# I tried some stuff but coding just sucks the life out of me
+# unless it's in MATLAB and then it's iffy
+# I'll attach this git link to my answers but I'm tired and don't see how I can
+# get a solution with 4 hours left with little knowledge of how coding works
+# I'm looking forward to the exam because I feel like I understand
+# the material better conceptually than I am able to code it. It's all greek to me
+
